e80cde27bc1befed40e9a58685d3392351a4e909
Maciej Budzyński <maciejb@virtualdesign.pl>
2022-04-13 23:14:49 +0200

    new version of react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider, CssBaseline, ScopedCssBaseline } from "@mui/material";
+import createEmotionCache from "../createEmotionCatche";
+import theme from "../theme";
+
+const cache = createEmotionCache();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <ScopedCssBaseline>
+                <Component {...pageProps} />
+            </ScopedCssBaseline>
+        </ThemeProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/theme.tsx
@@ -0,0 +1,50 @@
+import { createTheme } from "@mui/material";
+
+declare module "@mui/material/styles" {
+    interface Theme {
+        palette: {
+            primary: {
+                main: string;
+            };
+            secondary: {
+                main: string;
+            };
+        };
+    }
+}
+
+const defaultTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#000",
+        },
+        secondary: {
+            main: "#f00",
+        },
+    },
+});
+
+const theme = {
+    ...defaultTheme,
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    // fontSize: 10,
+                    backgroundColor: "#00f",
+                },
+            },
+        },
+    },
+
+    // overrides: {
+    //     MuiCssBaseline: {
+    //         body: {
+    //             fontSize: 10,
+    //             backgroundColor: "#000",
+    //         },
+    //     },
+    // },
+};
+
+export default theme;
