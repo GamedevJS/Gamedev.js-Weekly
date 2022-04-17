2b77ddeea576c146c56f50a7f843b31a790a2885
Maciej Budzyński <maciejb@virtualdesign.pl>
2022-04-17 10:46:22 +0200

    pages - templates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider, CssBaseline, ScopedCssBaseline } from "@mui/material";
-// import createEmotionCache from "../createEmotionCatche";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "../theme";
-import { useEffect } from "react";
 import useRemoveServerSideStyles from "../hooks/useRemoveServerSideStyles";
-
-// const cache = createEmotionCache();
 
 function MyApp({ Component, pageProps }: AppProps) {
     useRemoveServerSideStyles();
 
     return (
         <ThemeProvider theme={theme}>
-            {/* <ScopedCssBaseline> */}
-            <Component {...pageProps} />
             <CssBaseline />
-            {/* </ScopedCssBaseline> */}
+            <Component {...pageProps} />
         </ThemeProvider>
     );
 }
